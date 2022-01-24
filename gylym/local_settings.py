8972a06192260354b878f00259367752879e422d
Anvar Artemov <artemovanvar2@gmail.com>
2022-01-24 14:18:01 +0100

    Update local_settings.py

--- gylym/local_settings.py
+++ gylym/local_settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hello'
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
