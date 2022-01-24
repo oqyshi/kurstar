43785ebc722c26eae17bbf254935637febd77359
Anvar Artemov <artemovanvar2@gmail.com>
2022-01-24 14:17:47 +0100

    Update local_settings.py

--- gylym/local_settings.py
+++ gylym/local_settings.py
@@ -16,6 +16,6 @@ DATABASES = {
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
-# EMAIL_BACKEND = (
-#     'django.core.mail.backends.console.EmailBackend'
-# )
+EMAIL_BACKEND = (
+    'django.core.mail.backends.console.EmailBackend'
+)
